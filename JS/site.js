5ed029c6b346b7b28e67e717a894b7e0dfaafead
jmiraf <165960587+jmiraf@users.noreply.github.com>
2024-08-12 19:11:20 -0600

    Add files via upload

---
+++ JS/site.js
@@ -0,0 +1,43 @@
+﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+
+//functions to be called onmouseover and onmouseout to change the color of hello world
+function newColor() {
+    document.getElementById("hello_world").style.color = "blue";
+}
+
+function origColor() {
+    document.getElementById("hello_world").style.color = "darkmagenta";
+}
+
+//carousel javascript
+const slider = document.querySelector('.gallery');
+let isDown = false;
+let startX;
+let scrollLeft;
+
+slider.addEventListener('mousedown', e => {
+    isDown = true;
+    slider.classList.add('active');
+    startX = e.pageX - slider.offsetLeft;
+    scrollLeft = slider.scrollLeft;
+});
+slider.addEventListener('mouseleave', _ => {
+    isDown = false;
+    slider.classList.remove('active');
+});
+slider.addEventListener('mouseup', _ => {
+    isDown = false;
+    slider.classList.remove('active');
+});
+slider.addEventListener('mousemove', e => {
+    if (!isDown) return;
+    e.preventDefault();
+    const x = e.pageX - slider.offsetLeft;
+    const SCROLL_SPEED = 3;
+    const walk = (x - startX) * SCROLL_SPEED;
+    slider.scrollLeft = scrollLeft - walk;
+});
+
